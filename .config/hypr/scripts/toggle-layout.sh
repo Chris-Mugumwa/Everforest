1400c1e2c5ab23eabb4eb65c845f49f20a51ad83
Chris <chris.mugumwa@timbuktutravel.com>
2025-12-06 10:51:06 +0200

    Added hyprscroller instead of hyprscrolling

--- .config/hypr/scripts/toggle-layout.sh
+++ .config/hypr/scripts/toggle-layout.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# Toggle between hyprscroller (scrolling) and dwindle layouts
+# Toggle between hyprscroller and dwindle layouts
 
 current=$(hyprctl getoption general:layout -j | jq -r '.str')
 
-if [ "$current" = "scrolling" ]; then
+if [ "$current" = "scroller" ]; then
     hyprctl keyword general:layout dwindle
     notify-send "Layout" "Switched to Dwindle"
 else
-    hyprctl keyword general:layout scrolling
-    notify-send "Layout" "Switched to Scrolling"
+    hyprctl keyword general:layout scroller
+    notify-send "Layout" "Switched to Scroller"
 fi
