a31507d7baae50228338082101c114962dee7831
Chris <chris.mugumwa@timbuktutravel.com>
2025-12-07 10:53:41 +0200

    Gaming Rofi

--- .config/rofi/gamelauncher/launch.sh
+++ .config/rofi/gamelauncher/launch.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 STEAM_PATH="$HOME/.local/share/Steam/steamapps"
+ICON_PATH="$HOME/.local/share/Steam/appcache/librarycache"
 dir="$HOME/.config/rofi/gamelauncher"
 
-# Parse installed games from Steam's appmanifest files
+# Parse installed games with icons
 get_games() {
     for acf in "$STEAM_PATH"/appmanifest_*.acf; do
         if [[ -f "$acf" ]]; then
@@ -11,14 +12,29 @@ get_games() {
             appid=$(grep -Po '"appid"\s*"\K[^"]+' "$acf")
             # Skip Proton/Steam runtime entries
             [[ "$name" =~ ^(Proton|Steam\ Linux|Steamworks) ]] && continue
-            echo "$name|$appid"
+
+            # Find icon (try multiple formats)
+            icon=""
+            for ext in icon.jpg library_600x900.jpg header.jpg; do
+                if [[ -f "$ICON_PATH/${appid}_${ext}" ]]; then
+                    icon="$ICON_PATH/${appid}_${ext}"
+                    break
+                fi
+            done
+
+            echo "$name|$appid|$icon"
         fi
     done | sort
 }
 
-# Rofi menu
-selected=$(get_games | cut -d'|' -f1 | rofi -dmenu -i -p "Games" \
-    -theme "$dir/config.rasi")
+# Rofi menu with icons
+selected=$(get_games | while IFS='|' read -r name appid icon; do
+    if [[ -n "$icon" ]]; then
+        echo -en "$name\x00icon\x1f$icon\n"
+    else
+        echo "$name"
+    fi
+done | rofi -dmenu -i -p "Games" -show-icons -theme "$dir/config.rasi")
 
 [[ -z "$selected" ]] && exit 0
 
